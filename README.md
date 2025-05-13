5200e2759a2fe84d8b5c7c070eda2be883b057dd
Libor Pichler <lpichler@redhat.com>
2025-05-13 17:12:29 +0200

    Initial script

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # insights-service-deployer
+
+
+## Deploy rbac and HBI service with dedicated kafka connect stack (debezium included)
+
+git clone git@github.com:project-kessel/insights-service-deployer.git
+export EPHEMERAL_TOKEN=<> # get token from ephemeral cluster
+export EPHEMERAL_SERVER=<> # get server from ephemeral cluster
+./deploy.sh deploy
+
---
+++ deploy.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+set -e
+
+login() {
+  if [[ -z "${EPHEMERAL_TOKEN}" || -z "${EPHEMERAL_SERVER}" ]]; then
+    [[ -z "${EPHEMERAL_TOKEN}" ]] && echo " - EPHEMERAL_TOKEN is not set"
+    [[ -z "${EPHEMERAL_SERVER}" ]] && echo " - EPHEMERAL_SERVER is not set"
+    exit 1
+  fi
+
+  echo "Login.."
+  oc login --token="${EPHEMERAL_TOKEN}" --server=${EPHEMERAL_SERVER}
+}
+
+release_current_namespace() {
+  echo "Releasing current environment.."
+  login
+  bonfire namespace release `oc project -q`
+}
+
+deploy() {
+  login
+  echo "Deploying..."
+  HOST_GIT_COMMIT=$(echo $(git ls-remote https://github.com/RedHatInsights/insights-host-inventory HEAD) | cut -d ' ' -f1)
+  RBAC_UI_GIT_COMMIT=$(echo $(git ls-remote https://github.com/RedHatInsights/insights-rbac-ui HEAD) | cut -d ' ' -f1)
+  bonfire deploy host-inventory -F true -p host-inventory/RBAC_V2_FORCE_ORG_ADMIN=true \
+  -p host-inventory/CONSUMER_MQ_BROKER=rbac-kafka-kafka-bootstrap:9092  \
+  --set-template-ref host-inventory="${HOST_GIT_COMMIT}"  \
+  -p rbac/V2_APIS_ENABLED=True -p rbac/V2_READ_ONLY_API_MODE=False -p rbac/V2_BOOTSTRAP_TENANT=True \
+  -p rbac/REPLICATION_TO_RELATION_ENABLED=True -p rbac/BYPASS_BOP_VERIFICATION=True \
+  -p rbac/KAFKA_ENABLED=False -p rbac/NOTIFICATONS_ENABLED=False \
+  -p rbac/NOTIFICATIONS_RH_ENABLED=False \
+  --set-image-tag quay.io/redhat-services-prod/hcc-accessmanagement-tenant/insights-rbac=latest \
+  -p host-inventory/BYPASS_RBAC=false \
+  --set-image-tag quay.io/redhat-services-prod/rh-platform-experien-tenant/insights-rbac-ui="${RBAC_UI_GIT_COMMIT}"
+
+  setup_debezium
+}
+
+setup_debezium() {
+  echo "Debezium is setting up.."
+  download_debezium_configuration
+  cd scripts
+  chmod 0700 kafka.sh
+  ./kafka.sh -n `oc project -q`
+  cd ..
+}
+
+download_debezium_configuration() {
+  RAW_BASE_URL="https://raw.githubusercontent.com/RedHatInsights/insights-rbac/master"
+  FILES=(
+    "deploy/kafka-connect.yml"
+    "deploy/debezium-connector.yml"
+    "scripts/kafka.sh"
+    "scripts/connector-params.env"
+  )
+
+  RAW_BASE_URL="https://raw.githubusercontent.com/RedHatInsights/insights-rbac/master"
+
+  # --- DOWNLOAD ---
+  for FILE in "${FILES[@]}"; do
+    TARGET="./$FILE"
+    DIR_PATH=$(dirname "$TARGET")
+    FILE_NAME=$(basename "$FILE")
+    URL="$RAW_BASE_URL/$FILE"
+
+    mkdir -p "$DIR_PATH"
+
+    if [[ -f "$TARGET" ]]; then
+      echo "Skipping $FILE_NAME (already exists)"
+      continue
+    fi
+
+    echo "Downloading $FILE_NAME to $DIR_PATH"
+    curl -sSL -o "$TARGET" "$URL"
+    echo "Saved to $TARGET"
+  done
+}
+
+clean_download_debezium_configuration() {
+  rm -rf deploy
+  rm -rf scripts
+}
+
+usage() {
+  echo "Usage: $SCRIPT_NAME {release_current_namespace|deploy|clean_download_debezium_configuration}"
+  exit 1
+}
+
+case "$1" in
+  release_current_namespace)
+    release_current_namespace
+    ;;
+  deploy)
+    deploy
+    ;;
+  clean_download_debezium_configuration)
+    clean_download_debezium_configuration
+    ;;
+  *)
+    usage
+    ;;
+esac
